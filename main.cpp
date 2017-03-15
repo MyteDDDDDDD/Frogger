77e39743e41972977b84be41ef4a337c579341ea
MyteD <elvisyuan6988@gmail.com>
2017-03-16 01:09:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,6 +22,39 @@ int CMP(const void *a, const void *b) {
 		return -1;
 }
 
+int Find(int element,int A[]) {
+	if (A[element] < 0)		
+		return element;	
+	else		
+		return Find(A[element],A);
+}
+
+void UnionSet(int set1, int set2, int A[]) {
+	A[set1] += A[set2]; // A[ root of set ] is negative, and its
+	A[set2] = set1;     // magnitude is the num of members
+}
+
+void Union(int element1, int element2,int A[]) {
+	int root1 = Find(element1,A);
+	int root2 = Find(element2,A);
+	if (A[root1] < A[root2]) //root1 has more member		
+		UnionSet(root1, root2,A);	
+	else		
+		UnionSet(root2, root1,A);
+}
+
+bool ifContain(int a,int b,int A[]) {
+	int fa = Find(a, A);
+	int fb = Find(b, A);
+	if (fa == -1 || fb == -1)
+		return false;
+	else if (fa == fb)
+		return true;
+	else
+		return false;
+}
+
+
 double cal(int *stone[], int l) {//prim
 	int countE = 0;
 	for (int i = 0; i < l - 1; i++) {//from ith stone
@@ -40,22 +73,21 @@ double cal(int *stone[], int l) {//prim
 
 	qsort(All_Edges, (countE + 1), sizeof(Edge), CMP);
 
-	double d=0;
-	bool *nEdge=new bool[l];// l stones
-	for (int i = 0; i < countE+1; i++) {
-		int s = All_Edges[i].From;
-		int e = All_Edges[i].To;
-		if(nEdge[s]!=true||nEdge[s]== NULL)
-			nEdge[s] = true;
-		if(nEdge[e] != true || nEdge[e] == NULL)
-			nEdge[e] = true;
-		d = max(d,All_Edges[i].Length);
-		cout << d << endl;
-		if (nEdge[0] == true && nEdge[1] == true)//connect two frogs
-			return d;
-		
+	double d = 0;
+	int *nStone=new int[l];
+	for (int i = 0; i < l; i++) {
+		nStone[i] =-1;
 	}
-	return 0;
+	for (int i = 0; i < countE+1; i++) {
+		
+		if (ifContain(0,1,nStone)==false) {
+			Union(All_Edges[countE].From, All_Edges[countE].To, nStone);
+			d = max(d, All_Edges[i].Length);
+		}
+
+	}
+
+	return d;
 }
 int main() {
 	queue<double> c;
